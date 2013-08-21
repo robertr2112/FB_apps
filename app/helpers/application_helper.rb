33000605e04ea7008085eeab17474940fbf1ef5f
Robert Reynolds <robertr2112@yahoo.com>
2013-08-21 18:34:17 -0500

    Changed all references to Sample App

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
 
   # Return a title on a per-page basis
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-puts "page_title: #{page_title}"
-    if page_title.nil?
+    base_title = "Football Pool Mania"
+    if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,8 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_content('Football Pool Mania') }
     it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
