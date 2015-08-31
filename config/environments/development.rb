a1d16b2e1756d1f70d7ff019f9b2632457a1db59
Robert Reynolds <robertr2112@yahoo.com>
2015-08-30 23:20:29 -0500

    fixing a font-awesome problem with heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ FBApps::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+	
+	# Update paths for Font-Awesome
+	config.assets.paths << Rails.root.join(‘app’, ‘assets’, ‘fonts’)
+  config.assets.precompile += %w(.svg .eot .woff .ttf)
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
